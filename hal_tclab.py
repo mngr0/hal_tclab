0f5311d85213d51dc48e095e37aead38c7374174
mngr0 <ngrmarco@libero.it>
2019-05-05 03:52:51 -0400

    lot of work done

--- hal_tclab.py
+++ hal_tclab.py
@@ -9,8 +9,10 @@ setpoints=[0,0,0,0,0]
 for i in range(4):
   h.newpin("setpoint-"+str(i), hal.HAL_FLOAT, hal.HAL_IN)
   h.newpin("temperature-"+str(i), hal.HAL_FLOAT, hal.HAL_OUT)
+  h.newpin("enable-"+str(i),hal.HAL_BIT, hal.HAL_IN)
   h["temperature-%s"%str(i)] = 0
   h["setpoint-%s"%str(i)] = 0
+  h["enable-"+str(i)] = 0
 
 h.newpin("error", hal.HAL_BIT, hal.HAL_OUT)
 tc=tclab.TCLab()
@@ -25,13 +27,16 @@ try:
   #  print ("no connection")
   #  raise SystemExit
   while 1:
-    #h['out'] = h['in']
     for i in range(4):
       h["temperature-%s"%str(i)]=tc.temperature(i)
       tmp_set = h["setpoint-%s"%str(i)]
       if tmp_set != setpoints[i]:
         tc.setpoint(i,tmp_set)
-        #update required
+        setpoints[i] = tmp_set
+        if tmpset>10:
+          tc.enable(i)
+        else:
+          tc.disable(i)
     time.sleep(1)
 
 except KeyboardInterrupt:
--- therm_unify.py
+++ therm_unify.py
@@ -3,50 +3,47 @@
 import hal, time
 import sys
 
-def create_therm_unify(name):
+#def create_therm_unify(name):
 
 
 
 #import argparse
 #print (str(sys.argv))
-#h = hal.component(sys.argv[1])
-  h = hal.component(name)
+h = hal.component(sys.argv[1])
+#h = hal.component(name)
 #a input pin for every setpoint
 #a output pin for every temp
 
-  h.newpin("in.0", hal.HAL_FLOAT, hal.HAL_IN)
-  h.newpin("in.1", hal.HAL_FLOAT, hal.HAL_IN)
-  h.newpin("out" , hal.HAL_FLOAT, hal.HAL_OUT)
-  time.sleep(1)
+h.newpin("in.0", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("in.1", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("out" , hal.HAL_FLOAT, hal.HAL_OUT)
+time.sleep(1)
+old0 = h["in.0"]
+old1 = h["in.1"]
 
+
+#def therm_unify_routine():
+#  if( old0 != h["in.0"]):
+#    h["out"] = h["in.0"]
+#  if( old1 != h["in.1"]):
+#    h["out"] = h["in.1"]
+#  old0 = h["in.0"]
+#  old1 = h["in.1"]
+
+#export function
+h.ready()
+
+
+try:
   old0 = h["in.0"]
   old1 = h["in.1"]
-
-
-  def therm_unify_routine():
+  while 1:
     if( old0 != h["in.0"]):
       h["out"] = h["in.0"]
     if( old1 != h["in.1"]):
       h["out"] = h["in.1"]
     old0 = h["in.0"]
     old1 = h["in.1"]
-
-#export function
-  h.ready()
-
-
-  try:
-    old0 = h["in.0"]
-    old1 = h["in.1"]
-    while 1:
-      if( old0 != h["in.0"]):
-        h["out"] = h["in.0"]
-      if( old1 != h["in.1"]):
-        h["out"] = h["in.1"]
-      old0 = h["in.0"]
-      old1 = h["in.1"]
-
-      time.sleep(1)
-
-  except KeyboardInterrupt:
-      raise SystemExit
+    time.sleep(1)
+except KeyboardInterrupt:
+  raise SystemExit
