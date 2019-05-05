cad856321f802fe4f23b7ea6552dc1f7b7d241c4
mngr0 <ngrmarco@libero.it>
2019-05-05 04:01:47 -0400

    removed enable from hal pin

--- hal_tclab.py
+++ hal_tclab.py
@@ -9,35 +9,38 @@ setpoints=[0,0,0,0,0]
 for i in range(4):
   h.newpin("setpoint-"+str(i), hal.HAL_FLOAT, hal.HAL_IN)
   h.newpin("temperature-"+str(i), hal.HAL_FLOAT, hal.HAL_OUT)
-  h.newpin("enable-"+str(i),hal.HAL_BIT, hal.HAL_IN)
-  h["temperature-%s"%str(i)] = 0
-  h["setpoint-%s"%str(i)] = 0
-  h["enable-"+str(i)] = 0
+  #h.newpin("enable-"+str(i),hal.HAL_BIT, hal.HAL_IN)
+  h.newpin("error-"+str(i), hal.HAL_BIT, hal.HAL_OUT)
+  h["temperature-"+str(i)] = 0
+  h["setpoint-"+str(i)] = 0
+  #h["enable-"+str(i)] = False
+  h["error"+str(i)]= False
 
 h.newpin("error", hal.HAL_BIT, hal.HAL_OUT)
+h["error"] = 0
 tc=tclab.TCLab()
 
 h.ready()
 
 try:
-  #try:
   h["error"]=False
-  #except:
-  #  h["error"]=True
-  #  print ("no connection")
-  #  raise SystemExit
   while 1:
-    for i in range(4):
-      h["temperature-%s"%str(i)]=tc.temperature(i)
-      tmp_set = h["setpoint-%s"%str(i)]
-      if tmp_set != setpoints[i]:
-        tc.setpoint(i,tmp_set)
-        setpoints[i] = tmp_set
+    try:
+      for i in range(4):
+        h["temperature-%s"%str(i)]=tc.temperature(i)
+        tmp_set = h["setpoint-%s"%str(i)]
+        if tmp_set != setpoints[i]:
+          tc.setpoint(i,tmp_set)
+          setpoints[i] = tmp_set
         if tmpset>10:
           tc.enable(i)
+          h["error-"+str(i)] = False
         else:
           tc.disable(i)
-    time.sleep(1)
+          h["error-"+str(i)] = True
+      time.sleep(1)
+    except:
+     h["error"] = True
 
 except KeyboardInterrupt:
     raise SystemExit
