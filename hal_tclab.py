7b3d303a5c53172c650faf1eca2f4d5a0eeaee5e
mngr0 <ngrmarco@libero.it>
2019-05-15 11:48:20 -0400

    15/5/2019

--- hal_tclab.py
+++ hal_tclab.py
@@ -1,46 +1,65 @@
 #!/usr/bin/python2
 
-import hal, time
+from machinekit import hal
+import time
 import tclab
-h = hal.component("hal_tclab")
+
+class hal_tclab:
+  def __init__(self, name="hal_tclab"):
+    print("HERE IS HAL_TCLAB STARITNG")
+    self.h = hal.Component(name)
 #a input pin for every setpoint
 #a output pin for every temp
-setpoints=[0,0,0,0,0]
-for i in range(4):
-  h.newpin("setpoint-"+str(i), hal.HAL_FLOAT, hal.HAL_IN)
-  h.newpin("temperature-"+str(i), hal.HAL_FLOAT, hal.HAL_OUT)
-  #h.newpin("enable-"+str(i),hal.HAL_BIT, hal.HAL_IN)
-  h.newpin("error-"+str(i), hal.HAL_BIT, hal.HAL_OUT)
-  h["temperature-"+str(i)] = 0
-  h["setpoint-"+str(i)] = 0
-  #h["enable-"+str(i)] = False
-  h["error-"+str(i)]= False
+    self.setpoints=[0,0,0,0,0]
+    for i in range(4):
+      self.h.newpin("setpoint-"+str(i), hal.HAL_FLOAT, hal.HAL_IN)
+      self.h.newpin("temperature-"+str(i), hal.HAL_FLOAT, hal.HAL_OUT)
+      self.h.newpin("error-"+str(i), hal.HAL_BIT, hal.HAL_OUT)
+      self.h["temperature-"+str(i)] = 0
+      self.h["setpoint-"+str(i)] = self.setpoints[i]
+      self.h["error-"+str(i)]= False
+    self.h.newpin("enable",hal.HAL_BIT, hal.HAL_IN)
+    self.h.newpin("error", hal.HAL_BIT, hal.HAL_OUT)
+    self.h["error"] = 0
+    self.tc=tclab.TCLab()
+    self.h.ready()
+    print("HERE IS HAL_TCLAB READY")
 
-h.newpin("error", hal.HAL_BIT, hal.HAL_OUT)
-h["error"] = 0
-tc=tclab.TCLab()
 
-h.ready()
+  def take(self):
+    return self.h
 
-try:
-  h["error"]=False
-  while 1:
+  def routine(self):
     try:
-      for i in range(4):
-        h["temperature-%s"%str(i)]=tc.temperature(i)
-        tmp_set = h["setpoint-%s"%str(i)]
-        if tmp_set != setpoints[i]:
-          tc.setpoint(i,tmp_set)
-          setpoints[i] = tmp_set
-        if tmpset>10:
-          tc.enable(i)
-          h["error-"+str(i)] = False
-        else:
-          tc.disable(i)
-          h["error-"+str(i)] = True
-      time.sleep(1)
-    except:
-     h["error"] = True
+      self.h["error"]=False
+      while 1:
+        self.h["error"]=False
+        try:
+          for i in range(4):
+            self.h["temperature-"+str(i)]=self.tc.temperature(i)
+            tmp_set = self.h["setpoint-"+str(i)]
+            if tmp_set != self.setpoints[i]:
+              self.tc.setpoint(i,tmp_set)
+              self.setpoints[i] = tmp_set
+            if self.h["enable"]:
+              self.tc.enable(i)
+            else:
+              self.tc.disable(i)
+        except Exception as e:
+          self.h["error"] = True
+          print (str(e))
+    except Exception as e:
+      print( str(e))
+      raise SystemExit
 
-except KeyboardInterrupt:
-    raise SystemExit
+print("HERE IS HAL_TCLAB MAIN")
+
+comp= hal_tclab()
+#hal.addf("servo-thread")
+#try:
+#  while 1:
+#    comp.routine()
+#    time.sleep(1)
+#except Exception as e:
+#  print (str(e))
+#  raise SystemExit
--- therm_unify.py
+++ therm_unify.py
@@ -14,6 +14,7 @@ class therm_unify:
     self.h.newpin("out" , hal.HAL_FLOAT, hal.HAL_OUT)
     self.old0 = self.h["in.0"]
     self.old1 = self.h["in.1"]
+    self.h.ready()
 
   def routine():
     if( old0 != h["in.0"]):
@@ -29,34 +30,28 @@ if __name__ == '__NOmain__':
   comp=therm_unify(sys.argv[1])
 
 
-#import argparse
-#print (str(sys.argv))
-h = hal.component(sys.argv[1])
-#h = hal.component(name)
-#a input pin for every setpoint
-#a output pin for every temp
-
-h.newpin("in.0", hal.HAL_FLOAT, hal.HAL_IN)
-h.newpin("in.1", hal.HAL_FLOAT, hal.HAL_IN)
-h.newpin("out" , hal.HAL_FLOAT, hal.HAL_OUT)
+  h = hal.component(sys.argv[1])
+  h.newpin("in.0", hal.HAL_FLOAT, hal.HAL_IN)
+  h.newpin("in.1", hal.HAL_FLOAT, hal.HAL_IN)
+  h.newpin("out" , hal.HAL_FLOAT, hal.HAL_OUT)
 #time.sleep(1)
-old0 = h["in.0"]
-old1 = h["in.1"]
-
-#export function
-h.ready()
-
-
-try:
   old0 = h["in.0"]
   old1 = h["in.1"]
-  while 1:
-    if( old0 != h["in.0"]):
-      h["out"] = h["in.0"]
-    if( old1 != h["in.1"]):
-      h["out"] = h["in.1"]
+
+#export function
+  h.ready()
+
+
+  try:
     old0 = h["in.0"]
     old1 = h["in.1"]
-    time.sleep(1)
-except KeyboardInterrupt:
-  raise SystemExit
+    while 1:
+      if( old0 != h["in.0"]):
+        h["out"] = h["in.0"]
+      if( old1 != h["in.1"]):
+        h["out"] = h["in.1"]
+      old0 = h["in.0"]
+      old1 = h["in.1"]
+      time.sleep(1)
+  except KeyboardInterrupt:
+    raise SystemExit
